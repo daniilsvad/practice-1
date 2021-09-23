d0ba11ba84c16ee80ba13a389f26cc8dd3d7eb3b
daniilsvad <91286985+daniilsvad@users.noreply.github.com>
2021-09-23 22:23:16 +0300

    Add files via upload
    Added new python file with 1st practic

---
+++ program1.py
@@ -0,0 +1,19 @@
+#input
+name = input('input your Name: ') 
+surname = input('input your Surname: ')
+phoneNum = input('input your Phone number: ')
+street = input('input your Street name: ')
+houseNum = input('input your House number: ')
+apartmentNum = input('input your Apartment number: ')
+town = input('input your Town: ')
+index = input('input your Index: ')
+country = input('input your Country: ')
+
+#output
+print()
+print(f'{name} {surname}')
+print(f'{phoneNum}')
+print(f'Str. {street} {houseNum}, ap. {apartmentNum}, {town}')
+print(f'{index}')
+print(f'{country}')
+
